2c3636f7257c348c9754df7744445c85a25cdb6d
sky4no1 <105664118+sky4no1@users.noreply.github.com>
2024-08-03 00:48:17 +0300

    Update save12.lua

--- save12.lua
+++ save12.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService("HttpService")
 
 local SaveManager = {} do
-	SaveManager.Folder = "FluentSettings"
+	SaveManager.Folder = "HalalHub"
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
