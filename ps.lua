6fcec8a4e15fea248d3e30c326a4b3650e69fd42
sky4no1 <105664118+sky4no1@users.noreply.github.com>
2024-08-05 19:58:15 +0300

    Update ps.lua

--- ps.lua
+++ ps.lua
@@ -1,6 +1,5 @@
 local function skibidi()
   while true do
-  game:GetService("ReplicatedStorage").Events.reserved:InvokeServer("Skibidi")
   game:GetService("Players").LocalPlayer.PlayerGui.chooseType.Frame.RemoteEvent:FireServer(true)
   wait(0.1)
   end
