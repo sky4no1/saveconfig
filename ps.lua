007983034495a88070041aa3370c3943d4451c85
sky4no1 <105664118+sky4no1@users.noreply.github.com>
2024-08-05 20:11:54 +0300

    Update ps.lua

--- ps.lua
+++ ps.lua
@@ -1,9 +1,13 @@
 local function skibidi()
-  while true do
-  game:GetService("Players").LocalPlayer.PlayerGui.chooseType.Frame.RemoteEvent:FireServer(true)
-  wait(0.1)
-  end
+    local player = game:GetService("Players").LocalPlayer
+    local playerGui = player:WaitForChild("PlayerGui")
+    local chooseTypeFrame = playerGui:WaitForChild("chooseType").Frame
+    local remoteEvent = chooseTypeFrame:WaitForChild("RemoteEvent")
+
+    while true do
+        remoteEvent:FireServer(true)
+        wait(0.1)
+    end
 end
 
 skibidi()
-
