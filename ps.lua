7df33cfcb654092a8d73d8bac99bc6c34931cd57
sky4no1 <105664118+sky4no1@users.noreply.github.com>
2024-08-06 15:19:36 +0300

    Update ps.lua

--- ps.lua
+++ ps.lua
@@ -1,2 +1,4 @@
 local remote = game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("chooseType"):WaitForChild("Frame"):WaitForChild("RemoteEvent")
 remote:FireServer(true)
+local remote = game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("chooseType"):WaitForChild("Frame"):WaitForChild("RemoteEvent")
+remote:FireServer(true)
